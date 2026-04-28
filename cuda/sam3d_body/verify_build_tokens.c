a357a913609449236054627e2cfd32970deb689b
Syoyo Fujita <syoyo@lighttransport.com>
2026-04-28 11:58:08 +0900

    Wire CUDA SAM3D bf16 precision mode

--- cuda/sam3d_body/verify_build_tokens.c
+++ cuda/sam3d_body/verify_build_tokens.c
@@ -44,7 +44,7 @@ int main(int argc, char **argv)
     float threshold = 5e-4f;
     float mean_threshold = 1e-4f;
     int device = 0, verbose = 0;
-    const char *precision = "fp16";
+    const char *precision = "bf16";
 
     for (int i = 1; i < argc; i++) {
         if      (!strcmp(argv[i], "--safetensors-dir") && i+1 < argc) sft_dir = argv[++i];
@@ -59,7 +59,7 @@ int main(int argc, char **argv)
     if (!sft_dir || !refdir) {
         fprintf(stderr, "Usage: %s --safetensors-dir DIR --refdir DIR "
                         "[--threshold F] [--mean-threshold F] "
-                        "[--device N] [--precision fp16|bf16|fp32] [-v]\n",
+                        "[--device N] [--precision bf16|fp16] [-v]\n",
                 argv[0]);
         return 2;
     }
