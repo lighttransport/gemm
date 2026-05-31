7d9abb8146fa9bc7db0e9f95e32c2ed646738752
Syoyo Fujita <syoyo@lighttransport.com>
2026-05-31 16:21:35 +0900

    Borrow trellis2 FDG hash for PBR

--- cuda/trellis2/test_cuda_trellis2.c
+++ cuda/trellis2/test_cuda_trellis2.c
@@ -891,8 +891,9 @@ sparse_done:
                     fprintf(stderr, "\n=== PBR Texture Baking (res=%d) ===\n", tex_res);
 
                     double pbr_build_t0 = t2_harness_now_ms();
-                    t2_pbr_field pbr = t2_pbr_from_decoder_take(
-                        tex_result.feats, tex_result.coords, tex_result.N, tex_res);
+                    t2_pbr_field pbr = t2_pbr_from_decoder_take_with_hash(
+                        tex_result.feats, tex_result.coords, tex_result.N, tex_res,
+                        fdg_mesh.hash_keys, fdg_mesh.hash_vals, fdg_mesh.hash_cap);
                     tex_result.feats = NULL;
                     t2_harness_timing_log("pbr_field_build_cpu", pbr_build_t0);
                     t2_shape_dec_result_free(&tex_result);
