b8e2537445163f706f2147b5715337e73e651629
Syoyo Fujita <syoyo@lighttransport.com>
2026-08-30 11:01:08 +0900

    Expose fused mHC build switch

--- a64fx/glm5/build_glm53f_integrated_12n.sh
+++ a64fx/glm5/build_glm53f_integrated_12n.sh
@@ -24,6 +24,9 @@ fi
 if [ "${GLM53F_NO_MATH_ERRNO:-0}" = 1 ]; then
     cflags+=(-fno-math-errno)
 fi
+if [ "${GLM53F_MHC_FUSED:-0}" = 1 ]; then
+    cflags+=(-DGLM53F_MHC_FUSED=1)
+fi
 ldflags=(-L"$mpi_lib" -lmpi -lm -ltofucom)
 external=(-DGLM53F_EXTERNAL_ST_IMPLEMENTATION)
 
